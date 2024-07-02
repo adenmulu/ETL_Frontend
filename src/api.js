1f73f9de32a1e0abce8dd29c01e2a70208775e7d
adenmulu <108455516+adenmulu@users.noreply.github.com>
2024-07-02 14:51:05 +0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 // src/api.js
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3003';
+const API_URL = 'http://172.20.105.98  :3003';
 
 const api = axios.create({
   baseURL: API_URL,
